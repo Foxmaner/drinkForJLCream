9c48bedb7354e14ad6d59b1cc400641991483938
Axel Rönnberg <axero912@student.liu.se>
2024-01-27 00:01:59 +0100

    feat:(made adding players feature)

--- drinkForJLCream/src/components/WelcomeItem.vue
+++ drinkForJLCream/src/components/WelcomeItem.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
+
     <div class="details">
       <h3>
         <slot name="heading"></slot>
       </h3>
+      
       <slot></slot>
     </div>
   </div>
---
+++ drinkForJLCream/src/components/player.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+    <div class="item">
+        <p>
+            <slot>
+            </slot>
+        </p>
+    </div>
+</template>
