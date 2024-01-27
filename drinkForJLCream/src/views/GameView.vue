2de51b2e31ab7a94e6d0a5bccfbb0027342e3f24
Axel Rönnberg <axero912@student.liu.se>
2024-01-27 20:55:44 +0100

    fixed eskils shit

--- drinkForJLCream/src/views/GameView.vue
+++ drinkForJLCream/src/views/GameView.vue
@@ -1,14 +1,21 @@
-<script lang="ts">
+<script setup lang="ts">
     import Game from "../game.ts"
+ 
+    import cards from '../components/cards.vue'
+
     let g = new Game()
-    console.log(window.location.search + "aaaaa")
+
+    console.log(window.location.search)
     const params = window.location.search
         .slice(1)
-        .split('&')
-        .map(p => p.split('='))
-        .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {})
+        .split("&")
+        .map((p) => p.split("="))
+        .reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {});
+    console.log(params)
 
     for (let i in params) {
+        console.log(i)
+        console.log(params[i])
         g.addPlayer(i, params[i])
     }
 
@@ -17,50 +24,14 @@
     console.log("females: " + g.getFemales())
     console.log("nonbinaries: " + g.getNonBinaries())
 
-    // Import Swiper Vue.js components
-    import { Swiper, SwiperSlide } from 'swiper/vue';
 
-    // Import Swiper styles
-    import 'swiper/css';
-
-    import 'swiper/css/effect-cards';
-
-    import './style.css';
-
-    // import required modules
-    import { EffectCards } from 'swiper/modules';
-
-    export default {
-        components: {
-            Swiper,
-            SwiperSlide,
-        },
-        setup() {
-            return {
-                modules: [EffectCards],
-            };
-        },
-    };
 </script>
 
 
 <template>
-    <swiper
-      :effect="'cards'"
-      :grabCursor="true"
-      :modules="modules"
-      class="mySwiper"
-    >
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-      <swiper-slide>Slide 4</swiper-slide>
-      <swiper-slide>Slide 5</swiper-slide>
-      <swiper-slide>Slide 6</swiper-slide>
-      <swiper-slide>Slide 7</swiper-slide>
-      <swiper-slide>Slide 8</swiper-slide>
-      <swiper-slide>Slide 9</swiper-slide>
-    </swiper>
-  </template>
+
+    <cards></cards>
+
+</template>
 
 
