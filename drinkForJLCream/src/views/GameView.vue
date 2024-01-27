dbcaf34a6464d3782a8a9cb43b2d0ebdc6777b6f
Eskil Brännerud <eskil.brann@gmail.com>
2024-01-27 18:50:35 +0100

    sum cards

--- drinkForJLCream/src/views/GameView.vue
+++ drinkForJLCream/src/views/GameView.vue
@@ -1,28 +1,41 @@
-<script setup lang="ts">
-
-    import Gamecard from '../components/GameCard.vue'
-
-    import Game from "../game.ts"
-
-    let g: Game = new Game()
-
-    const item = this.$router.params.item
-
-</script>
-
-
-
-
 <template>
-    <p>Starta spelet</p>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-    <Gamecard title="Game 1" description="Björn dricker 8 klunkar"/>
-</template>
+    <swiper
+      :effect="'cards'"
+      :grabCursor="true"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+      <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+      <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+    </swiper>
+  </template>
+  <script>
+    // Import Swiper Vue.js components
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+  
+    // Import Swiper styles
+    import 'swiper/css';
+  
+    import 'swiper/css/effect-cards';
+  
+    import './style.css';
+  
+    // import required modules
+    import { EffectCards } from 'swiper/modules';
+  
+    export default {
+      components: {
+        Swiper,
+        SwiperSlide,
+      },
+      setup() {
+        return {
+          modules: [EffectCards],
+        };
+      },
+    };
+  </script>
+  
