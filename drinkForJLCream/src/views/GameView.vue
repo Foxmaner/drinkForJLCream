b19e92e495e0a5789e384217d0a3141fdc86822d
Eskil Brännerud <eskil.brann@gmail.com>
2024-01-27 14:56:02 +0100

    changed card a little

--- drinkForJLCream/src/views/GameView.vue
+++ drinkForJLCream/src/views/GameView.vue
@@ -1,15 +1,10 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import Gamecard from '../components/GameCard.vue'
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+
+
+
+<template>
+    <Gamecard title="Game 1" description="Björn dricker 8 klunkar" />
+</template>
